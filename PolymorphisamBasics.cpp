ebfe671cf672a74abc88eea0592a9acb9b402cee
Mangesh Gaherwar <32591553+dotnetgik@users.noreply.github.com>
2022-03-28 09:58:23 +0530

    Add files via upload

---
+++ PolymorphisamBasics.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+	int a,b,c;
+	a=10;
+	b=10;
+	c=a+b; // Adding Two Numbers 
+	cout<<c<<endl;
+	
+	string firstName,LastName;
+	firstName="Bruce ";
+	LastName="Banner ";
+	cout<<"Hulk's original Name is " <<firstName+LastName; // Concatenating the Two String 
+	
+}
